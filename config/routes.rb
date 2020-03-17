b1ad1c7a76cfc802288956e52e6910db6267ae1e
Mehdi Mosbah <mosbah.meh.didi@gmail.com>
2020-03-17 14:45:45 +0100

    routes corrigées

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'pins#index'
   get 'dashboard', to: 'pages#dashboard'
-  resources :pins do
-    resources :comments, only: [:create, :new, :show, :destroy, :update, :edit]
-    resources :votes, only: [:create, :new, :index]
+  resources :comments, only: [:destroy, :update, :edit]
+  resources :pins, except: :index do
+    resources :comments, only: [:index, :create, :new]
+    resources :votes, only: [:create, :new]
   end
 
-  resources :visits, only: [:create, :new]
+  resources :visits, only: [:create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
