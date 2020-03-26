2a18c0214545d7271f82b01404723fcd19b03f6a
Mehdi Mosbah <mosbah.meh.didi@gmail.com>
2020-03-26 15:19:46 +0100

    sedd

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,40 +20,30 @@ manu = User.create!(username: 'Manu', email: 'manu@mail.fr', password: 'password
 puts "#{User.count} users created"
 
 
-  csv_options = { col_sep: "\t", quote_char: '"', :headers => true }
-  filepath    = "#{Rails.root}/db/poi_3.csv"
+#   csv_options = { col_sep: "\t", quote_char: '"', :headers => true }
+#   filepath    = "#{Rails.root}/db/poi_3.csv"
 
-    CSV.foreach(filepath, csv_options) do |row|
-      Pin.create!(
-        title: row["name"],
-        description: row["type"],
-        latitude: row["@lat"],
-        longitude: row["@lon"],
-        user: [yvan, manu, mehdi].sample
-      )
-      puts "created #{row['name']}"
-end
+#     CSV.foreach(filepath, csv_options) do |row|
+#       Pin.create!(
+#         title: row["name"],
+#         description: row["type"],
+#         latitude: row["@lat"],
+#         longitude: row["@lon"],
+#         user: [yvan, manu, mehdi].sample
+#       )
+#       puts "created #{row['name']}"
+# end
 
-pins = Pin.all
-puts "#{Pin.count} pins created"
+# pins = Pin.all
+# puts "#{Pin.count} pins created"
 
-30.times do
-  Vote.create!(
-    note: rand(0...5),
-    pin: pins.sample,
-    user: [yvan, manu, mehdi].sample
-    )
-end
-puts "#{Vote.count} votes created"
+# 30.times do
+#   Vote.create!(
+#     note: rand(0...5),
+#     pin: pins.sample,
+#     user: [yvan, manu, mehdi].sample
+#     )
+# end
+# puts "#{Vote.count} votes created"
 
-20.times do
-  Comment.create!(
-    content: ["c'est jolie", "ouais bof", "ça n'a rien à voir mais aller l'om",
-      "j'ai vu la même mais autre part", "a quand la bière ?",
-      "hum... je ne sais pas quoi dire", "ça va toi ?"].sample,
-      user: [yvan, manu, mehdi].sample,
-      pin: pins.sample
-    )
-end
-
-puts "#{Comment.count} comments created"
+# puts "#{Comment.count} comments created"
